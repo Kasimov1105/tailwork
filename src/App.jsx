2c4f989b9caddbaf39e73f51e95a7a3babde54a2
kasimov <kasimovfazliddin0@gmail.com>
2024-07-16 09:19:38 +0500

    code new

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// App.jsx
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Features from './pages/features/Features';
+import Pricing from './pages/pricing/Pricing';
+import FAQ from './pages/faq/FAQ';
+import Blogs from './pages/blogs/Blogs';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+        iubfuygu
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+import OurProject from './OurProject'
+import Frequently from './Frequently '
+
+function Home() {
+  return (
+    <div>
+      <Hero/>
+      <OurProject/>
+      <Frequently/>
+    </div>
+  )
+}
+
+export default Home
